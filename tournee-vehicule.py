50a0978b0013556d5a718db3fe6150f5c45b6b93
durandth <theophane.durand@etu.utc.fr>
2021-11-25 12:07:43 +0100

    ça avance

--- tournee-vehicule.py
+++ tournee-vehicule.py
@@ -1,4 +1,5 @@
 import json
+import numpy as np
 
 path = "./data.json"
 
@@ -17,12 +18,67 @@ def get_customers(jsonObject):
 def get_home(jsonObject):
     return jsonObject['home']
 
+def get_areas(customers, nb_trucks):
+    nb_areas = min(nb_trucks, len(customers))
+    centers = []
+    for n in range(nb_areas):
+        centers.append(customers[n])
+
+    #boucle à partir de là
+    areas = [[]]*nb_areas
+    old_areas = None
+    # On sépare l'espace actuel en nb_areas espaces
+    while areas != old_areas:
+        old_areas = areas
+        for customer in customers:
+            distances = [0]*nb_areas
+            for i, center in enumerate(centers):
+                distance = distance_between_customers(customer, center)
+                distances[i] = distance
+            index = get_index_of_min(distances)
+            areas[index].append(customer)
+            
+
+        for n in range(nb_areas):
+            center[n] = get_area_center(areas[n])
+    return areas
+
+def get_area_center(l):
+    x = 0
+    y = 0
+    y_value_sum = 0
+    x_value_sum = 0
+    g_sum = 0
+    for customer in l:
+        x_value_sum += customer['x']*customer['value']
+        y_value_sum += customer['y']*customer['value']
+        g_sum += customer['value']
+
+    x = x_value_sum / g_sum
+    y = y_value_sum / g_sum
+    return {'x' : x,'y' : y}
+
+def distance_between_customers(c1, c2):
+    c1_coord = np.array((c1['x'], c1['y']))
+    c2_coord = np.array((c2['x'], c2['y']))
+    d = np.linalg.norm(c1_coord-c2_coord)
+    return d
+
+def get_index_of_min(l):
+    temp = min(l)
+    return [i for i, j in enumerate(l) if j == temp][0]
+
+def accessible_customers(customers, trucks):
+    # éliminer les clients inaccessibles
+    return 0
+
 if __name__ == '__main__':
     jsonObject = format_json(path)
     trucks = get_trucks(jsonObject)
+    # customers = accessible_customers(get_customers(jsonObject), trucks)
     customers = get_customers(jsonObject)
     home = get_home(jsonObject)
 
     nb_vehicules = trucks['amount']
 
-    
+    print(get_areas(customers, nb_vehicules))

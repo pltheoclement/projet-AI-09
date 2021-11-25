d390acc5738326b40063b1e5cf1621e4187b10dd
durandth <theophane.durand@etu.utc.fr>
2021-11-25 18:42:36 +0100

    c'est fini

--- tournee-vehicule.py
+++ tournee-vehicule.py
@@ -18,7 +18,7 @@ def get_customers(jsonObject):
 def get_home(jsonObject):
     return jsonObject['home']
 
-def get_areas(customers, nb_trucks, home):
+def get_areas(customers, nb_trucks):
     nb_areas = min(nb_trucks, len(customers))
     centers = []
     for n in range(nb_areas):
@@ -38,10 +38,8 @@ def get_areas(customers, nb_trucks, home):
                 distances[i] = distance
             index = get_index_of_min(distances)
             areas[index].append(customer)
-            print(areas)
-            print('\n')
         for n in range(nb_areas):
-            center[n] = get_area_center(areas[n])
+            centers[n] = get_area_center(areas[n])
     return areas
 
 def get_area_center(l):
@@ -57,7 +55,10 @@ def get_area_center(l):
 
     x = x_value_sum / g_sum
     y = y_value_sum / g_sum
-    return {'x' : x,'y' : y}
+    center = dict()
+    center['x'] = x
+    center['y'] = y
+    return center
 
 def distance_between_customers(c1, c2):
     c1_coord = np.array((c1['x'], c1['y']))
@@ -69,17 +70,81 @@ def get_index_of_min(l):
     temp = min(l)
     return [i for i, j in enumerate(l) if j == temp][0]
 
-def accessible_customers(customers, trucks):
-    # éliminer les clients inaccessibles
-    return 0
+def remove_unaccessible_customers(customers, trucks, home):
+    accessible_customers = []
+    for customer in customers:
+        if (2*distance_between_customers(home, customer) <= trucks['distance']):
+            accessible_customers.append(customer)
+    return accessible_customers
+
+def coef_reduce_value(area, home):
+    values = []
+    distances = []
+    for customer in area:
+        values.append(customer['value'])
+        distances.append(distance_between_customers(customer, home))
+
+    
+    min_value = max(values)
+    min_distance = max(distances)
+    print("\n")
+    print(min_distance / min_value)
+    print("\n")
+    print("\n")
+    return min_distance / min_value
+
+def ppv(c, list_of_available_customers, coef):
+    dist = 0
+    choice = None
+    for customer in list_of_available_customers:
+        euclid_dist = distance_between_customers(c, customer)
+        new_dist = (coef * customer['value']) / euclid_dist
+        if new_dist > dist:
+            dist = new_dist
+            choice = customer
+    return choice
+
+def path_in_area(area, home, trucks):
+    list_of_available_customers = area
+    current_point = home
+    fuel = trucks['distance']
+    coef = coef_reduce_value(area, home)
+    path = [current_point]
+    total_value = 0
+    while (fuel >= distance_between_customers(current_point, home)) and (len(list_of_available_customers) > 0):
+        pot_next = ppv(current_point, area, coef)
+        if fuel - (distance_between_customers(current_point, pot_next) + distance_between_customers(pot_next, home)) > 0 :
+            fuel -= distance_between_customers(current_point, pot_next)
+            current_point = pot_next
+            total_value += current_point['value']
+            list_of_available_customers.remove(current_point)
+            path.append(current_point)
+        else :
+            list_of_available_customers.remove(pot_next)
+    fuel -= distance_between_customers(current_point, home)
+    path.append(home)
+    return total_value, path
+
+def main(customers, home, trucks):
+    areas = get_areas(customers, trucks['amount'])
+    total_value = 0
+    all_paths = []
+    for area in areas:
+        [value, path] = path_in_area(area, home, trucks)
+        total_value += value
+        all_paths.append(path)
+    return total_value, all_paths
+
 
 if __name__ == '__main__':
     jsonObject = format_json(path)
     trucks = get_trucks(jsonObject)
-    # customers = accessible_customers(get_customers(jsonObject), trucks)
-    customers = get_customers(jsonObject)
     home = get_home(jsonObject)
+    customers = remove_unaccessible_customers(get_customers(jsonObject), trucks, home)
 
-    nb_vehicules = trucks['amount']
+    [total_value, all_paths] = main(customers, home, trucks)
 
-    print(get_areas(customers, nb_vehicules, home))
+    print(total_value)
+    print('\n')
+
+    print(all_paths)

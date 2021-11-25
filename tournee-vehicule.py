c12c713f1a30590f99a0fa44b78966d47c16b2ed
durandth <theophane.durand@etu.utc.fr>
2021-11-25 10:51:29 +0100

    début du script et récupération des données depuis le json

--- tournee-vehicule.py
+++ tournee-vehicule.py
@@ -1,1 +1,26 @@
 import json
+
+path = "./data.json"
+
+def format_json(path):
+	with open(path, encoding="utf8") as jsonFile:
+	    jsonObject = json.load(jsonFile)
+	    jsonFile.close()
+	return jsonObject
+
+def get_trucks(jsonObject):
+    return jsonObject['trucks']
+
+def get_customers(jsonObject):
+    return jsonObject['customers']
+
+def get_home(jsonObject):
+    return jsonObject['home']
+
+if __name__ == '__main__':
+    jsonObject = format_json(path)
+    trucks = get_trucks(jsonObject)
+    customers = get_customers(jsonObject)
+    home = get_home(jsonObject)
+
+    nb_vehicules = trucks['amount']

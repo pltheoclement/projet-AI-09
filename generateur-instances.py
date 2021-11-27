62371ac4c5e751c125e24c88b0f6717d4e690c5b
durandth <theophane.durand@etu.utc.fr>
2021-11-27 18:37:03 +0100

    ajout d'un générateur d'instances et d'un nouveau fichier d'instances

---
+++ generateur-instances.py
@@ -0,0 +1,73 @@
+import json
+import random
+
+# chaque point peut être placé sur une grille de 20x20 allant de 0 à 20 en x et en y
+# Chaque instance contient un seul dépot appelé home
+# Chaque instance peut contenir de 1 à 100 clients appelés customers
+# Chaque instance peut contenir de 1 à 20 véhicules appelés trucks
+# Les camions de chaque instance peuvent parcourir une distance de 10 à 90
+# Chaque client à une valeur pouvant aller de 5 à 300
+
+GRID = 20
+MIN_CUSTOMERS = 1
+MAX_CUSTOMERS = 100
+MIN_TRUCKS = 1
+MAX_TRUCKS = 20
+MIN_VALUE = 5
+MAX_VALUE = 300
+MIN_DISTANCE = 10
+MAX_DISTANCE = 90
+MAX_SEED = 999999999
+
+def get_random_home(grille):
+    home = {
+        "x": random.randint(0, grille),
+        "y": random.randint(0, grille)
+    }
+    return home
+
+def get_random_trucks(min_trucks, max_trucks, min_distance, max_distance):
+    trucks = {
+        "amount": random.randint(min_trucks, max_trucks),
+        "distance": random.randint(min_distance, max_distance)
+    }
+    return trucks
+
+def get_random_customers(grille, home, min_customers, max_customers, min_value, max_value):
+    nb_customers = random.randint(min_customers, max_customers)
+    customers = []
+    unavailble_places = [(home["x"], home["y"])]
+    while len(customers) < nb_customers:
+        attemp = (random.randint(0, grille), random.randint(0, grille))
+        if attemp not in unavailble_places:
+            unavailble_places.append(attemp)
+            customers.append({
+                "x": attemp[0],
+                "y": attemp[1],
+                "value": random.randint(min_value, max_value)
+            })
+    return customers
+
+def create_instance(number, grille, min_customers, max_customers, min_value, max_value, min_trucks, max_trucks, min_distance, max_distance):
+    random.seed(number)
+    home = get_random_home(grille)
+    trucks = get_random_trucks(min_trucks, max_trucks, min_distance, max_distance)
+    customers = get_random_customers(grille, home, min_customers, max_customers, min_value, max_value)
+
+    instance = {
+        "home": home,
+        "trucks": trucks,
+        "customers": customers
+    }
+    return instance
+
+def create_multiple_instances(nb_instances):
+    instances = {}
+    for i in range(nb_instances):
+        instances[i] = create_instance(i, GRID, MIN_CUSTOMERS, MAX_CUSTOMERS, MIN_VALUE, MAX_VALUE, MIN_TRUCKS, MAX_TRUCKS, MIN_DISTANCE, MAX_DISTANCE)
+    return instances
+
+if __name__ == "__main__":
+    instances = create_multiple_instances(50)
+    jsonObject = json.dumps(instances)
+    print(jsonObject)
